4fb9753272be62c3ba91ed93e575545d330ecd38
Max Kovaljov <shirioko@gmail.com>
2013-12-11 14:46:45 +0100

    Removed 100 continue header
    Should fix error 417

--- WART/frmRegister.cs
+++ WART/frmRegister.cs
@@ -31,6 +31,7 @@ namespace WART
         public frmRegister()
         {
             InitializeComponent();
+            ServicePointManager.Expect100Continue = false;
             ServicePointManager.ServerCertificateValidationCallback += CustomCertificateValidation;
         }
 
